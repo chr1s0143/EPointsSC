b1498a4aedcb348ea50419921aff4f25ca24bb66
chr1s0143 <christopherhallch94@gmail.com>
2014-09-13 02:15:36 +0100

    added command "/epreload" to reload the config file in "epoints.class"

--- src/me/chr1s0143/EPointsSC/epoints.java
+++ src/me/chr1s0143/EPointsSC/epoints.java
@@ -2,11 +2,14 @@ package me.chr1s0143.EPointsSC;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import java.io.File;
 /**
  * Created by chris on 11/09/2014.
  */
@@ -19,6 +22,9 @@ public class epoints extends JavaPlugin {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
+            if (cmd.getName().equalsIgnoreCase("epreload"))
+                reloadConfig();
+            sender.sendMessage(ChatColor.GREEN + "EPoints Config Reloaded!");
             if (cmd.getName().equalsIgnoreCase("epoints")) {
                 Player player = (Player) sender;
                 if (player.hasPermission("epoints.use"))
@@ -27,8 +33,7 @@ public class epoints extends JavaPlugin {
                     else if (args.length == 1) {
                         getServer().dispatchCommand(getServer().getConsoleSender(), "enjin addpoints " + args[0] + " " + getConfig().getInt("Points"));
 
-                        player.sendMessage(ChatColor.GOLD + "2 Points Added to " + ChatColor.GREEN + args[0] + "'s" + ChatColor.GOLD + " Enjin Account!");
-
+                        player.sendMessage(ChatColor.GOLD + " " + getConfig().getInt("Points") + " Points Added to " + ChatColor.GREEN + args[0] + "'s" + ChatColor.GOLD + " Enjin Account!");
 
                     }
             }
