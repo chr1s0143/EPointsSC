66ce6af1b2f88e5edcafeb6254bccf21b0198bc0
chr1s0143 <christopherhallch94@gmail.com>
2014-09-15 20:50:06 +0100

    epreload
    Fixed - "You do not have permission to use this command" message from
    appearing when the command /epreload was executed WITH permission. Only
    shows when the player does not have permission

--- src/me/chr1s0143/EPointsSC/epoints.java
+++ src/me/chr1s0143/EPointsSC/epoints.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 /**
  * Created by chris on 11/09/2014.
  */
@@ -24,10 +25,11 @@ public class epoints extends JavaPlugin {
             Player player = (Player) sender;
             if (commandLabel.equalsIgnoreCase("epreload")) {
                 if (player.hasPermission("epoints.reload"))
-                    reloadConfig();
-                player.sendMessage(ChatColor.DARK_GREEN + "Config has been reloaded!");
-            }
-           else if (commandLabel.equalsIgnoreCase("epoints")) {
+                    player.sendMessage(ChatColor.DARK_GREEN + "Config File Has Been Reloaded!");
+                else player.sendMessage(ChatColor.DARK_RED + "You do not have permission to use this command!");
+                reloadConfig();
+                }
+            else if (commandLabel.equalsIgnoreCase("epoints")) {
                 if (player.hasPermission("epoints.use"))
                     if (args.length == 0)
                         player.sendMessage(ChatColor.DARK_GREEN + player.getName() + ", try doing " + ChatColor.GOLD + "/epoints {player name}");
