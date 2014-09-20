7323ce0b8e5b1abeff3b2ab19067debc6d5e7ce6
chr1s0143 <christopherhallch94@gmail.com>
2014-09-20 14:28:42 +0100

    Added player online check
    If a player is NOT online it will notify the command sender that they
    are not online. OR the name is wrong (if player is online)

--- src/me/chr1s0143/EPointsSC/epoints.java
+++ src/me/chr1s0143/EPointsSC/epoints.java
@@ -37,9 +37,12 @@ public class epoints extends JavaPlugin {
                     if (args.length == 0)
                         player.sendMessage(ChatColor.DARK_GREEN + player.getName() + ", try doing " + ChatColor.GOLD + "/epoints {player name}");
                     else if (args.length == 1) {
-                        getServer().dispatchCommand(getServer().getConsoleSender(), "enjin addpoints " + args[0] + " " + getConfig().getInt("Points"));
+                        if (player.getServer().getPlayer(args[0]) != null) {
+                            Player targetPlayer = player.getServer().getPlayer(args[0]);
+                            getServer().dispatchCommand(getServer().getConsoleSender(), "enjin addpoints " + args[0] + " " + getConfig().getInt("Points"));
 
-                        player.sendMessage(ChatColor.GOLD + " " + getConfig().getInt("Points") + " Points Added to " + ChatColor.GREEN + args[0] + "'s" + ChatColor.GOLD + " Enjin Account!");
+                            player.sendMessage(ChatColor.GOLD + " " + getConfig().getInt("Points") + " Points Added to " + ChatColor.GREEN + args[0] + "'s" + ChatColor.GOLD + " Enjin Account!"); }
+                        else player.sendMessage(ChatColor.GOLD + "Player " + ChatColor.DARK_GREEN + args[0] + ChatColor.DARK_RED + " is not currently online. Please wait until they are online before using this command!" );
                     }
                 } else {
                     player.sendMessage(ChatColor.DARK_RED + "You do not have permission to use this command!");
